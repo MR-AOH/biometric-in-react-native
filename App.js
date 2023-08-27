00925c0264df5e11c0e6d9608f3956f673835890
MR-AOH <63532911+MR-AOH@users.noreply.github.com>
2023-08-27 19:18:13 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,55 +1,71 @@
-import { Button, View, TextInput, StyleSheet } from "react-native";
-import { useState } from "react";
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import * as LocalAuthentication from "expo-local-authentication";
 
-export default function DisableInput() {
-  const [isDisabled, setIsDisabled] = useState(false);
+export default function App() {
+  const authenticateWithBiometric = async () => {
+    try {
+      const isBiometricAvailable = await LocalAuthentication.hasHardwareAsync();
+
+      if (isBiometricAvailable) {
+        const supportedTypes =
+          await LocalAuthentication.supportedAuthenticationTypesAsync();
+
+        if (
+          supportedTypes.includes(
+            LocalAuthentication.AuthenticationType.FINGERPRINT
+          )
+        ) {
+          // Fingerprint authentication for Android
+          const result = await LocalAuthentication.authenticateAsync({
+            promptMessage: "Authenticate to access your account.",
+          });
+
+          if (result.success) {
+            alert("Authentication Successful");
+          } else {
+            alert("Authentication Failed");
+          }
+        } else if (
+          supportedTypes.includes(
+            LocalAuthentication.AuthenticationType.FACIAL_RECOGNITION
+          )
+        ) {
+          // Face ID authentication for iOS
+          const result = await LocalAuthentication.authenticateAsync({
+            promptMessage: "Authenticate to access your account.",
+            disableDeviceFallback: true, // Prevents fallback to PIN/passcode
+          });
+
+          if (result.success) {
+            alert("Authentication Successful");
+          } else {
+            alert("Authentication Failed");
+          }
+        } else {
+          alert("Biometric authentication is not available on this device.");
+        }
+      } else {
+        alert("Biometric hardware is not available on this device.");
+      }
+    } catch (error) {
+      console.error("Biometric authentication error:", error);
+    }
+  };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.inputs}
-          placeholder="First Name"
-          underlineColorAndroid="transparent"
-          editable={!isDisabled}
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.inputs}
-          placeholder="Last Name"
-          underlineColorAndroid="transparent"
-          editable={!isDisabled}
-        />
-      </View>
-      <Button
-        title={isDisabled ? "Enable Input" : "Disable Input"}
-        onPress={() => setIsDisabled(!isDisabled)}
-      />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ marginBottom: 20 }}>Biometric Authentication Example</Text>
+      <TouchableOpacity
+        onPress={authenticateWithBiometric}
+        style={{
+          backgroundColor: "blue",
+          padding: 10,
+          borderRadius: 5,
+        }}
+      >
+        <Text style={{ color: "white" }}>Authenticate with Biometric</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#FFFFFF",
-    padding: 20,
-  },
-  inputContainer: {
-    borderColor: "#000000",
-    borderWidth: 0.5,
-    borderRadius: 10,
-    backgroundColor: "#FFFFFF",
-    height: 45,
-    width: "100%",
-    marginVertical: 10,
-    paddingHorizontal: 10,
-  },
-  inputs: {
-    flex: 1,
-    padding: 10,
-  },
-});
